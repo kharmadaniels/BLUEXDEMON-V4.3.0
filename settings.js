a93506e27917c1e9d902edb1d76d313491ca1858
kharmadaniels <kharmadaniel72@gmail.com>
2025-01-17 19:48:32 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const version = require("@whiskeysockets/baileys/package.json").version
 
 
 
-//2348038903353
-global.pairingNumber = "2348038903353"
+//2348035461340
+global.pairingNumber = "2348035461340"
 
 
 
